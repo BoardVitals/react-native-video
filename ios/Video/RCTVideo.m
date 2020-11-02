31a7c3f1a15ad7770fecc381f977a0543f825eec
Javier Jareno Diaz <javier@boardvitals.com>
2020-11-02 15:43:52 -0500

    - Fix for BVM-859: controls not showing if starting paused, changing paused logic to start playing and then pausing

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -860,17 +860,16 @@ static int const RCTVideoUnset = -1;
 
 - (void)setPaused:(BOOL)paused
 {
+  if([_ignoreSilentSwitch isEqualToString:@"ignore"]) {
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+  } else if([_ignoreSilentSwitch isEqualToString:@"obey"]) {
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];
+  }
+  [_player play];
+  [_player setRate:_rate];
   if (paused) {
     [_player pause];
     [_player setRate:0.0];
-  } else {
-    if([_ignoreSilentSwitch isEqualToString:@"ignore"]) {
-      [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
-    } else if([_ignoreSilentSwitch isEqualToString:@"obey"]) {
-      [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];
-    }
-    [_player play];
-    [_player setRate:_rate];
   }
   
   _paused = paused;
